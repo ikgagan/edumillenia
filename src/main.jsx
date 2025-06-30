2c28fad2618d1c72414333c545efe1a38e2c0ffb
ikgagan <ik.gagan@gmail.com>
2025-06-30 00:03:01 -0500

    Configure custom domain edumillenia.com

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/edumillenia">
+    <BrowserRouter basename="/">
       <ThemeProvider>
         <App />
       </ThemeProvider>
